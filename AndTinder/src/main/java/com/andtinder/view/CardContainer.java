1655748179ae7b86e6c010e27b16e7bc926ceafa
Enrique López Mañas <eenriquelopez@gmail.com>
2016-02-21 12:51:02 +0100

    Pull request  #61, updated tools, deactivated testCoverage

--- AndTinder/src/main/java/com/andtinder/view/CardContainer.java
+++ AndTinder/src/main/java/com/andtinder/view/CardContainer.java
@@ -484,11 +484,11 @@ public class CardContainer extends AdapterView<ListAdapter> {
             if (mTopCard != null)
                 mTopCard.setLayerType(LAYER_TYPE_HARDWARE, null);
 
-            if (cardModel.getOnCardDimissedListener() != null) {
+            if (cardModel.getOnCardDismissedListener() != null) {
                 if (targetX < 0) {
-                    cardModel.getOnCardDimissedListener().onDislike();
+                    cardModel.getOnCardDismissedListener().onDislike();
                 } else {
-                    cardModel.getOnCardDimissedListener().onLike();
+                    cardModel.getOnCardDismissedListener().onLike();
                 }
             }
 
